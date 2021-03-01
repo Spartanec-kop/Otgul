df24f62d23f7b7ddb9b55e33f261ef419efc2c23
Варнавский Павел <spartanec_kop@mail.ru>
2021-03-01 19:33:09 +0300

    test commit

--- Otgul.UI/src/components/employees/EmployeesToggledTable.vue
+++ Otgul.UI/src/components/employees/EmployeesToggledTable.vue
@@ -1,8 +1,61 @@
 <template lang="pug">
-  div раскрывающаяся таблица
+  div
+    FilteredTable(
+      :tableData="users"
+      :columns="columns"
+    )
 </template>
 <script>
+import { mapState, mapActions } from 'vuex'
+import FilteredTable from '../filteredTable/FilteredTable'
 export default {
-  name: 'EmployeesToggledTable'
+  name: 'EmployeesToggledTable',
+  components: { FilteredTable },
+  data () {
+    return {
+      columns: [
+        {
+          field: 'login',
+          name: 'Логин',
+          filtered: true
+        },
+        {
+          field: 'lastName',
+          name: 'Фамилия',
+          filtered: true
+        },
+        {
+          field: 'firstName',
+          name: 'Имя',
+          filtered: true
+        },
+        {
+          field: 'middleName',
+          name: 'Отчество',
+          filtered: true
+        },
+        {
+          field: 'roleName',
+          name: 'Роль',
+          filtered: true
+        },
+        {
+          name: 'Действия',
+          type: 'action'
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapState('users', {
+      users: state => state.users
+    })
+  },
+  methods: {
+    ...mapActions('users', ['fetchUsers'])
+  },
+  mounted () {
+    this.fetchUsers()
+  }
 }
 </script>
